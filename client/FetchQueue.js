608e21f5383e70fd3a6de83a43f6c043451e7816
Anwesh43 <anweshthecool0@gmail.com>
2017-05-15 17:39:40 +0530

    adding request class

--- client/FetchQueue.js
+++ client/FetchQueue.js
@@ -1,7 +1,39 @@
 class FetchQueue {
     constructor() {
-        this.queue = []
+        this.requestQueue = []
     }
-    addRequest(url,cb) {
+    addRequest(url,successCb,errorCb,headers,resType {
+        const request = new Request(url,successCb,errorCb,headers,resType)
+        this.requestQueue.push(request)
+        if(this.requestQueue.length == 1) {
+            processAllRequests()
+        }
+    }
+    processAllRequests() {
+      
+    }
+}
+class Request {
+    constructor(url,successCb,errorCb,headers,resType) {
+        this.url = url
+        this.successCb = successCb
+        this.errorCb = errorCb
+        this.headers = headers
+        this.resType = resType
+    }
+    process(cb) {
+        fetch(this.url,{headers:this.headers}).then((res)=>{
+            let resPromise = res.text()
+            if(this.resType == 'json') {
+                resPromise = res.json()
+            }
+            retrun resPromise
+        }).then((result)=>{
+            this.successCb(result)
+            cb()
+        }).catch((err)=>{
+            this.errorCb(err)
+            cb()
+        })
     }
 }
